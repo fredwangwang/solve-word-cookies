6e6490f746a848371668602bb74c16eaeedb0810
Huan Wang <fredwanghuan@gmail.com>
2018-05-20 21:16:08 -0600

    added a poor man's progress bar

--- create_mapping.py
+++ create_mapping.py
@@ -2,22 +2,21 @@ from __future__ import print_function
 import sys
 import string
 import itertools
-try:
-    import cPickle as pickle
-except:
-    import pickle
+import pickle
 
 from helper import classify, isword
+
 
 i = 0
 mapping = dict()
 words = [line.rstrip('\n') for line in open(sys.argv[1], "r")]
+print()
 for word in words:
     i += 1
-    if i % 10000 == 0:
+    if i % 1000 == 0:
         # somehow print(str, flush=True) doesn't work in python2 even if
         # __future__.print_function is imported. This is a workaround
-        print(".", end='')
+        print("\033[AProcessed " + str(i) + " words...")
         sys.stdout.flush()
 
     if not isword(word):
@@ -29,5 +28,6 @@ for word in words:
     if category not in mapping:
         mapping[category] = list()
     mapping[category].append(word)
+print("\033[AFinished! " + str(i) + " words processed")
 
 pickle.dump(mapping, open("words_mapping.p", 'wb'))
